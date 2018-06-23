1970d88c3c2bdb596950b645c7f38b2d69cc61ac
erxz <22479344+erxz@users.noreply.github.com>
2018-06-23 19:48:45 +0200

    home page test

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="bg-section">
+    <div class="big-heart mx-auto">
+      <svg width="24" height="21" xmlns="http://www.w3.org/2000/svg"><path d="M11.608 20.997c-22.647-12.354-6.268-27.713 0-17.369 6.269-10.344 22.649 5.015 0 17.369z" fill="#010002" fill-rule="nonzero"/></svg>
+    </div>
+    <div class="intro mx-auto">
+      <div class="intro-item--left">
+        <h1>Alberto & Lucia</h1>
+      </div>
+      <div class="intro-item--center">
+        <img src="../assets/images/alber-lucia.jpg" alt="Alberto y Lucía dándose la mano" class="intro-img">
+      </div>
+      <div class="intro-item--right">
+        <p class="type-italic">Nos hace muy felices celebrar nuestra boda junto a vosotros. Completa el formulario y confirma si asistiras!</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<!-- If I have to write more css I prefer have another separate file -->
+<style src="../scss/main.scss" scoped></style>
