b9753c22a020572798ebbdcf6b4444ae3704523c
erxz <22479344+erxz@users.noreply.github.com>
2018-07-01 12:50:25 +0200

    iframe map

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -35,6 +35,6 @@ describe('Home', () => {
   })
   test('has correct content', () => {
     const wrapper = mount(Home)
-    expect(wrapper.text()).toBe(`Alberto & Lucia  Nos hace muy felices celebrar nuestra boda junto a vosotros. Completa el formulario y confirma si asistirás! completar  ver en google maps ¿Dónde? Villa María Bonita  Crevillente   ¿Cuándo? 6 de Octubre del 2018  18.30h  ¿nos ayudáis con la luna de miel? Banco XXX  ES6000491500051234567892`)
+    expect(wrapper.text()).toBe(`Alberto & Lucia  Nos hace muy felices celebrar nuestra boda junto a vosotros. Completa el formulario y confirma si asistirás! completar  ver en google maps ¿Dónde? Villa María Bonita  Crevillente   ¿Cuándo? 6 de Octubre del 2018  18.30h  ¿nos ayudáis con la luna de miel? ES3600811580380006184030`)
   })
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,16 +24,16 @@
     <section class="where">
       <div class="container container--two where-bg">
         <div class="row">
-          <div class="col right">
+          <div class="col where-col--left">
             <img src="~@/assets/images/location2x.png" alt="Foto de Villa María" width="395" class="where-img">
             <div class="where-link"><a href="https://goo.gl/maps/aM4tvNE5Yz52" class="uppercase" rel="noopener" target="_blank">ver en google maps</a></div>
           </div>
-          <div class="col where-col--right">
+          <div class="col col--right">
             <h6 class="uppercase">¿Dónde?</h6>
             <h2>Villa María Bonita <br> Crevillente</h2>
             <div class="divider"></div>
-            <div class="where-map">
-               <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d100276.89466781069!2d-0.8830823!3d38.2411596!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd63b94322204949%3A0x49368d13b65285ea!2zVmlsbGEgIk1hcsOtYSBCb25pdGEi!5e0!3m2!1sen!2ses!4v1530359510883" width="440" height="220" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <div class="where-map google-map">
+               <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d100276.89466781069!2d-0.8830823!3d38.2411596!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd63b94322204949%3A0x49368d13b65285ea!2zVmlsbGEgIk1hcsOtYSBCb25pdGEi!5e0!3m2!1sen!2ses!4v1530359510883" frameborder="0" allowfullscreen></iframe>
             </div>
           </div>
         </div>
@@ -48,13 +48,13 @@
             <div class="divider mx-auto"></div>
           </div>
         </div>
-        <div class="row">
+        <div class="row p-t-spacer">
           <div class="col">
             <h4 class="uppercase">¿nos ayudáis con la luna de miel?</h4>
-            <h3>Banco XXX <br> ES6000491500051234567892</h3>
+            <h3>ES3600811580380006184030</h3>
             <div class="divider"></div>
           </div>
-          <div class="col">
+          <div class="col col--right">
             <img src="~@/assets/images/honeymoon.png" alt="Imagen de una isla paradisíaca" class="">
           </div>
         </div>
