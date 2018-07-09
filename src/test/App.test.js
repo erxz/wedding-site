89c943987a3a07c4ebda8cc94ff0da2bf1d0a92e
erxz <22479344+erxz@users.noreply.github.com>
2018-07-09 21:53:07 +0200

    home page test content updated

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,23 +1,21 @@
 import {
   mount
 } from '@vue/test-utils'
+
+import Vue from 'vue'
+import router from '../router'
 import App from '../components/App.vue'
+
+// Components
 import Footer from '../components/Footer.vue'
 import Construction from '../components/Construction.vue'
+
+// Partials
 import Home from '../views/Home.vue'
 
-// describe('Footer', () => {
-//   test('is a Vue instance', () => {
-//     const wrapper = mount(Footer)
-//     expect(wrapper.isVueInstance()).toBeTruthy()
-//   })
-
-//   test('has correct content', () => {
-//     const wrapper = mount(Footer)
-//     expect(wrapper.text()).toBe(`Alber & Lucía
-//    Design and code by enreckless`)
-//   })
-// })
+// Plugins
+import VueLazyload from 'vue-lazyload';
+Vue.use(VueLazyload)
 
 // Test content based on route
 describe('Construction', () => {
@@ -27,14 +25,31 @@ describe('Construction', () => {
   })
 })
 
-// And more..
+// Home page content
 describe('Home', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(Home)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
+
   test('has correct content', () => {
     const wrapper = mount(Home)
-    expect(wrapper.text()).toBe(`Alberto & Lucia  Nos hace muy felices celebrar nuestra boda junto a vosotros. Completa el formulario y confirma si asistirás! completar  ver en google maps ¿Dónde? Villa María Bonita  Crevillente   ¿Cuándo? 6 de Octubre del 2018  18.30h  ¿nos ayudáis con la luna de miel? ES3600811580380006184030`)
+    expect(wrapper.text()).toBe(`Alberto & Lucia  Nos hace muy felices celebrar nuestra boda junto a vosotros. Completa el formulario y confirma si asistirás! completar  ¿Dónde? Villa María Bonita
+             Crevillente   ¿Cuándo? 6 de Octubre del 2018
+             18.30h  ¿nos ayudáis con la luna de miel? ES3600811580380006184030   Aquí os dejamos algunas fotos de preboda, después
+             subiremos las de la boda así que... no olvidéis enviarnos las vuestras!`)
   })
 })
+
+// // Footer
+// describe('Footer', () => {
+//   test('is a Vue instance', () => {
+//     const wrapper = mount(Footer)
+//     expect(wrapper.isVueInstance()).toBeTruthy()
+//   })
+//   test('has correct content', () => {
+//     const wrapper = mount(Footer)
+//     expect(wrapper.text()).toBe(`Alber & Lucía
+//     Design and code by enreckless`)
+//   })
+// })
